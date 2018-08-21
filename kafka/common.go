34ded4078d3a0b6993ef01e80620abba89d5acce
mb <barabash.michael@gmail.com>
2018-08-21 13:23:26 +0300

    kafka pipe added

--- kafka/common.go
+++ kafka/common.go
@@ -3,6 +3,7 @@ package kafka
 import (
 	"time"
 
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/lastexile/kepler"
 	"github.com/streadway/amqp"
 )
@@ -15,3 +16,13 @@ const connectionRetryInterval = 10 * time.Second
 
 // ConnectionFactoryFunc returns opened connection
 type ConnectionFactoryFunc func() (*amqp.Connection, error)
+
+func ensureDeliveryTimeouts(config *kafka.ConfigMap) *kafka.ConfigMap {
+	config.SetKey("api.version.request", "false")
+	config.SetKey("message.timeout.ms", 100)
+	config.SetKey("session.timeout.ms", 100)
+	config.SetKey("request.timeout.ms", 100)
+	config.SetKey("message.send.max.retries", 3)
+
+	return config
+}
--- kafka/sink.go
+++ kafka/sink.go
@@ -12,68 +12,72 @@ import (
 func NewSink(topic string, config *kafka.ConfigMap, formatter MarshallerFunc) (sink kepler.Sink, err error) {
 
 	var p *kafka.Producer
-
-	config.SetKey("api.version.request", "false")
-	config.SetKey("message.timeout.ms", 100)
-	config.SetKey("session.timeout.ms", 100)
-	config.SetKey("request.timeout.ms", 100)
-	config.SetKey("message.send.max.retries", 3)
 	p, err = kafka.NewProducer(config)
 	if err != nil {
 		log.Errorf("Unable to create kafka Producer: %v\n", err)
 		return
 	}
 
-	// TODO: consider closing of producer
-	// Delivery report handler for produced messages
-	go func() {
-		for e := range p.Events() {
-			switch ev := e.(type) {
-			case *kafka.Message:
-				if ev.TopicPartition.Error != nil {
-					log.Errorf("Delivery failed: %v\n", ev.TopicPartition)
-				} else {
-					log.Debugf("Delivered message to %v", ev.TopicPartition)
-				}
-			}
-		}
-	}()
-
 	delivery := make(chan kafka.Event)
-	sink = kepler.NewSink(topic, func(m kepler.Message) {
-		value, err := formatter(m)
-		if err != nil {
-			log.Error("Unable to marshall message")
-			return
-		}
-		msg := &kafka.Message{
-			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-			Value:          value,
-		}
-		for {
-			if err := p.Produce(msg, delivery); err != nil {
-				log.Errorf("Cannot enqueue message: %v\n", err)
-				return
-			}
 
-			//TODO track status synchronously
-			e := <-delivery
-
-			switch ev := e.(type) {
-			case *kafka.Message:
-				if ev.TopicPartition.Error != nil {
-					log.Errorf("Delivery failed: %v\n", ev.TopicPartition)
-					time.Sleep(connectionRetryInterval)
-					continue
-
-				} else {
-					log.Debugf("Delivered message to %v", ev.TopicPartition)
-					return
-				}
-			}
-		}
-
-	})
+	f := func(m kepler.Message) {
+		writePipe(m, topic, p, formatter, delivery)
+	}
+	sink = kepler.NewSink(topic, f)
 
 	return
+}
+
+// NewPipe creates transit kafka write pipe
+func NewPipe(topic string, config *kafka.ConfigMap, formatter MarshallerFunc) (pipe kepler.Pipe, err error) {
+
+	var p *kafka.Producer
+	p, err = kafka.NewProducer(ensureDeliveryTimeouts(config))
+	if err != nil {
+		log.Errorf("Unable to create kafka Producer: %v\n", err)
+		return
+	}
+
+	delivery := make(chan kafka.Event)
+
+	f := func(m kepler.Message) kepler.Message {
+		return writePipe(m, topic, p, formatter, delivery)
+	}
+	pipe = kepler.NewPipe(topic, f)
+
+	return
+}
+
+func writePipe(m kepler.Message, topic string, p *kafka.Producer, formatter MarshallerFunc, delivery chan kafka.Event) kepler.Message {
+	value, err := formatter(m)
+	if err != nil {
+		log.Error("Unable to marshall message")
+		return nil
+	}
+	msg := &kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          value,
+	}
+	for {
+		if err := p.Produce(msg, delivery); err != nil {
+			log.Errorf("Cannot enqueue message: %v\n", err)
+			return nil
+		}
+
+		//TODO track status synchronously
+		e := <-delivery
+
+		switch ev := e.(type) {
+		case *kafka.Message:
+			if ev.TopicPartition.Error != nil {
+				log.Errorf("Delivery failed: %v\n", ev.TopicPartition)
+				time.Sleep(connectionRetryInterval)
+				continue
+
+			} else {
+				log.Debugf("Delivered message to %v", ev.TopicPartition)
+				return m
+			}
+		}
+	}
 }
